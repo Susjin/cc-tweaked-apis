9f4e869ee17511ae5d196aaf4e49e980ff214000
Susjin <pedrohgmoreira@gmail.com>
2026-05-03 00:53:34 -0300

    Allow the changing of colors
    Changed 'Buttons' to 'ButtonsManager' as it fits more nicely
    Added heading and label functions
    Added a check for even lenght texts, making then begin writing one pixel earlier
    Added the 'checkClick' function, which executes the button's function if it's clicked

--- ButtonAPI/button.lua
+++ ButtonAPI/button.lua
@@ -1,47 +1,53 @@
 ---@class Button Main button class
 ---@field name string Name of the button to be indexed
 ---@field text string Text to be shown on the button (smaller than x2-x1)
+---@field func function Function to be executed when the button is pressed (always put the argument self as first, because it will always pass itself)
 ---@field x1 integer Button's starting X position
 ---@field x2 integer Button's finishing X position
 ---@field y1 integer Button's starting Y position
 ---@field y2 integer Button's finishing Y position
----@field colorInactive ccTweaked.colors.color (Optional) Background color of the button, default is colors.red
----@field colorActive ccTweaked.colors.color (Optional) Background color of the button when activated, default is colors.lime 
+---@field textColor ccTweaked.colors.color Background color of the button text, default is colors.white
+---@field colorInactive ccTweaked.colors.color Background color of the button, default is colors.red
+---@field colorActive ccTweaked.colors.color Background color of the button when activated, default is colors.lime 
 ---@field state boolean Current state of the button. True for active and false for inactive
----@field parent Buttons Parent of the button, responsible for it's rendering
+---@field manager Buttons Parent of the button, responsible for it's rendering
 local Button = {}
 Button.__intex = Button
 
 ---Creates a new Button
 ---@param name string Name of the button to be indexed
 ---@param text string Text to be shown on the button (smaller than x2-x1)
+---@param func function Function to be executed when the button is pressed (always put the argument self as first, because it will always pass itself)
 ---@param x1 integer Button's starting X position
 ---@param x2 integer Button's finishing X position
 ---@param y1 integer Button's starting Y position
 ---@param y2 integer Button's finishing Y position
----@param colorInactive ccTweaked.colors.color (Optional)   Background color of the button, default is colors.red
----@param colorActive ccTweaked.colors.color (Optional) Background color of the button when activated, default is colors.lime 
+---@param textColor ccTweaked.colors.color? (Optional) Color of the button text, default is colors.white
+---@param colorInactive ccTweaked.colors.color? (Optional) Background color of the button, default is colors.red
+---@param colorActive ccTweaked.colors.color? (Optional) Background color of the button when activated, default is colors.lime 
 ---@return Button
-function Button.new(name, text, x1, x2, y1, y2, colorInactive, colorActive)
+function Button.new(name, text, func, x1, x2, y1, y2, textColor, colorInactive, colorActive)
     ---@class Button
     local b = setmetatable({}, Button)
-    
+
     b.name = name
     b.text = text
+    b.func = func
     b.x1 = x1
     b.x2 = x2
     b.y1 = y1
     b.y2 = y2
+
+    b.textColor = textColor or colors.white
     b.colorInactive = colorInactive or colors.red
     b.colorActive = colorActive or colors.lime
 
     b.state = false
-    b.parent = nil
-
+    b.manager = nil
     return b
 end
 
-
+   
 local mon = peripheral.wrap("top")
 if !mon then return end
 
@@ -51,65 +57,42 @@ local button={}
 mon.setBackgroundColor(colors.black)
  
 function clearTable()
-   button = {}
-   mon.clear()
+    button = {}
+    mon.clear()
 end
-               
+
 function setTable(name, func, xmin, xmax, ymin, ymax)
-   button[name] = {}
-   button[name]["func"] = func
-   button[name]["active"] = false
-   button[name]["xmin"] = xmin
-   button[name]["ymin"] = ymin
-   button[name]["xmax"] = xmax
-   button[name]["ymax"] = ymax
+    button[name] = {}
+    button[name]["func"] = func
+    button[name]["active"] = false
+    button[name]["xmin"] = xmin
+    button[name]["ymin"] = ymin
+    button[name]["xmax"] = xmax
+    button[name]["ymax"] = ymax
 end
- 
+
 function funcName()
-   print("You clicked buttonText")
+    print("You clicked buttonText")
 end
-        
+
 function fillTable()
-   setTable("ButtonText", funcName, 5, 25, 4, 8)
+    setTable("ButtonText", funcName, 5, 25, 4, 8)
 end     
- 
- 
+
+
 function toggleButton(name)
-   button[name]["active"] = not button[name]["active"]
-   screen()
+    button[name]["active"] = not button[name]["active"]
+    screen()
 end     
- 
+
 function flash(name)
-   toggleButton(name)
-   screen()
-   sleep(0.15)
-   toggleButton(name)
-   screen()
+    toggleButton(name)
+    screen()
+    sleep(0.15)
+    toggleButton(name)
+    screen()
 end
-                                             
-function checkxy(x, y)
-   for name, data in pairs(button) do
-      if y>=data["ymin"] and  y <= data["ymax"] then
-         if x>=data["xmin"] and x<= data["xmax"] then
-            data["func"]()
-            return true
-            --data["active"] = not data["active"]
-            --print(name)
-         end
-      end
-   end
-   return false
-end
-     
-function heading(text)
-   w, h = mon.getSize()
-   mon.setCursorPos((w-string.len(text))/2+1, 1)
-   mon.write(text)
-end
-     
-function label(w, h, text)
-   mon.setCursorPos(w, h)
-   mon.write(text)
-end
+
+
 
 return Button
--- ButtonAPI/buttons.lua
+++ ButtonAPI/buttons.lua
@@ -3,43 +3,63 @@ local Button = require("button.lua")
 ---@class Buttons
 ---@field buttons Button[] An array of buttons
 ---@field monitor ccTweaked.peripherals.Monitor? Monitor to display the buttons
-local Buttons = {}
-Buttons.__index = Buttons
+---@field defaultBGColor ccTweaked.colors.color Default background color of the monitor
+---@field defaultTextColor ccTweaked.colors.color Default text color of the monitor
+local ButtonsManager = {}
+ButtonsManager.__index = ButtonsManager
 
 ---Creates a new list of buttons
 ---@param monitor string Name of the monitor or side it is attached
+---@param defaultBackgroundColor ccTweaked.colors.color? (Optional) Default background color of the monitor (default to black)
+---@param defaultTextColor ccTweaked.colors.color? (Optional) Default text color of the monitor (default to white)
 ---@return Buttons
-function Buttons.new(monitor)
-    local bts = setmetatable({}, Buttons)
+function ButtonsManager.new(monitor, defaultBackgroundColor, defaultTextColor)
+    local bts = setmetatable({}, ButtonsManager)
     bts.buttons = {}
     bts.monitor = peripheral.wrap(monitor)
+    bts.defaultBGColor = defaultBackgroundColor or colors.black
+    bts.defaultTextColor = defaultTextColor or colors.white
 
     return bts
 end
 
+---Checks if a given text has a even lenght. Returns the lenght and if it's even
+---@param text string Text to be checked
+---@return integer lenght Lenght of that text
+---@return integer sum Number to sum when checking where to place text
+function ButtonsManager.checkText(text)
+    local lenght = string.len(text)
+    local even = lenght % 2 == 0
+    return lenght, even and 0 or 1
+end
+
 ---Adds a button to the list of buttons
 ---@param button Button
-function Buttons:addToList(button)
+function ButtonsManager:addToList(button)
     self.buttons[button.name] = button
-    self.buttons[button.name].parent = self
+    self.buttons[button.name].manager = self
 end
 
 ---Fills a button on a given place of the screen
 ---@param button Button The button to be filled on the screen
----@param customDefaultBackgroundColor ccTweaked.colors.color? (Optional) Can set the background color to a different one than black
-function Buttons:fillButton(button, customDefaultBackgroundColor)
+function ButtonsManager:fillButton(button)
     if !self.monitor then printError("Monitor not set/found!"); return; end
+    
+    local oldTextColor = self.monitor.getTextColor()
 
     local buttonBackgroundColor = button.colorInactive
     if button.state then buttonBackgroundColor = button.colorActive end
     self.monitor.setBackgroundColor(buttonBackgroundColor)
-    
+
+    local lenght, sum = self.checkText(button.text)
+
     local yspot = math.floor((button.y1 + button.y2) / 2) --Position relatively to the monitor size (eg. '3' is the third pixel of the monitor on the Y level)
-    local xspot = math.floor((button.x2 - button.x1 - string.len(button.text)) / 2) + 1 --Position relatively to the button size (eg. '4' is the fourth pixel of the Button on the X level)
+    local xspot = math.floor((button.x2 - button.x1 - lenght) / 2) + sum --Position relatively to the button size (eg. '4' is the fourth pixel of the Button on the X level)
     for i = button.y1, button.y2 do
         self.monitor.setCursorPos(button.x1, i)
+        self.monitor.setTextColor(button.textColor)
         if i == yspot then
-            for j = 0, button.x2 - button.x1 - string.len(button.text) + 1 do --Used to get the total of blank spaces + the first letter of the text
+            for j = 0, button.x2 - button.x1 - lenght + 1 do --Used to get the total of blank spaces + the first letter of the text
                 if j == xspot then
                     self.monitor.write(button.text)
                 else
@@ -47,24 +67,60 @@ function Buttons:fillButton(button, customDefaultBackgroundColor)
                 end
             end
         else
-            for j = button.x1, button.x2 do
+            for _ = button.x1, button.x2 do
                 self.monitor.write(" ")
             end
         end
     end
 
-    self.monitor.setBackgroundColor(customDefaultBackgroundColor or colors.black)
+    self.monitor.setTextColor(oldTextColor)
+    self.monitor.setBackgroundColor(self.defaultBGColor)
 end
 
 ---Fills the screen with all buttons in the current list
-function Buttons:fillButtons()
+function ButtonsManager:fillButtons()
     for _, buttonData in pairs(self.buttons) do
         local buttonBackgroundColor = buttonData.colorInactive
         self:fillButton(buttonData)
     end
 end
 
+---Given a X and Y coordinate, checks if it was inside of one of the buttons, if so, execute the funcion and returns true
+---@param x integer X coordinate of the clicked pixel of the monitor
+---@param y integer Y coordinate of the clicked pixel of the monitor
+---@return boolean
+function ButtonsManager:checkClick(x, y)
+    for _, button in pairs(self.buttons) do
+        if x >= button.x1 and x <= button.x2  then
+            if y >= button.x1 and y <= button.x2 then
+               button:func()
+               return true
+            end
+        end
+    end
+    return false
+end
+
+function ButtonsManager:heading(text)
+    local width, _ = self.monitor.getSize()
+    local lenght, sum = self.checkText(text)
+    local oldTextColor = self.monitor.getTextColor()
+    
+    self.monitor.setCursorPos((width - lenght) / 2 + sum, 1)
+    self.monitor.setTextColor(self.defaultTextColor)
+    self.monitor.write(text)
+    self.monitor.setTextColor(oldTextColor)
+end
+
+function ButtonsManager:label(x, y, text)
+    local oldTextColor = self.monitor.getTextColor()
+
+    self.monitor.setCursorPos(x, y)
+    self.monitor.setTextColor(self.defaultTextColor)
+    self.monitor.write(text)
+    self.monitor.setTextColor(oldTextColor)
+end
 
 
 
-return Buttons
+return ButtonsManager
